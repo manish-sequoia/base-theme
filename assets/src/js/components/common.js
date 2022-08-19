ffc30237fd4988efcecf8ba8c34a1f54e4ac57e3
Manish <dev70.india@sequoiacap.com>
2022-08-19 17:43:39 +0530

    Fix code formatting

--- assets/src/js/components/common.js
+++ assets/src/js/components/common.js
@@ -11,31 +11,25 @@ const common = {
 	 * @return {void}
 	 */
 	init() {
-
 		this.primaryMenu();
-
 	},
 
 	primaryMenu() {
-
 		// Get all the link elements within the primary menu.
 		let i, len;
 
-		const menu = document.querySelector( '.primary-navigation' )
+		const menu = document.querySelector( '.primary-navigation' );
 
 		if ( ! menu ) {
-
 			return false;
-
 		}
 
 		const links = menu.getElementsByTagName( 'a' );
 
 		// Each time a menu link is focused or blurred, toggle focus.
 		for ( i = 0, len = links.length; i < len; i++ ) {
-
-			links[i].addEventListener( 'focus', toggleFocus, true );
-			links[i].addEventListener( 'blur', toggleFocus, true );
+			links[ i ].addEventListener( 'focus', toggleFocus, true );
+			links[ i ].addEventListener( 'blur', toggleFocus, true );
 		}
 
 		// Sets or removes the .focus class on an element.
@@ -44,25 +38,19 @@ const common = {
 
 			// Move up through the ancestors of the current link until we hit .primary-menu.
 			while ( -1 === self.className.indexOf( 'primary-menu' ) ) {
-
 				// On li elements toggle the class .focus.
 				if ( 'li' === self.tagName.toLowerCase() ) {
-
-					if ( - 1 !== self.className.indexOf( 'focus' ) ) {
-
+					if ( -1 !== self.className.indexOf( 'focus' ) ) {
 						self.className = self.className.replace( ' focus', '' );
-
 					} else {
-
 						self.className += ' focus';
-
 					}
 				}
 
 				self = self.parentElement;
 			}
 		}
-	}
+	},
 };
 
 /**
