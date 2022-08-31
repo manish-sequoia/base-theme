c81872b5cd031894957148881a9c4636e3adbb42
rashmi-sc <104899006+rashmi-sc@users.noreply.github.com>
2022-08-31 15:16:31 +0530

    Category Page

---
+++ category.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * The template for displaying Blog category.
+ *
+ * @package Base-Theme
+ */
+get_header();
+$queried_object = get_queried_object(); 
+$bt_category_image = get_field( 'image_cat',  $queried_object);
+if ( ! empty( $bt_category_image ) ) {
+	$base_theme_cat_image  = wp_get_attachment_url( $bt_category_image, 'full' );
+}
+?>
+<div class="grid-container">
+	<div class="grid-x grid-margin-x">
+		<div class="cell small-12 medium-8 large-8">
+			<div id="primary">
+				<main id="main" class="site-main" role="main">
+                              <div class="tagList">
+                                    <?php $tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'base-theme' ) );
+                                          if ( $tags_list ) {
+                                                /* translators: 1: list of tags. */
+                                                printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'base-theme' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                                          } 
+                                    ?>
+                              </div>
+					<?php if ( have_posts() ) { ?>
+
+						<header class="page-header">
+                                          <div class="bt-category-flex">
+                                                <div class="bt-cat-column">
+                                                      <?php
+                                                            the_archive_title( '<h1 class="page-title">', '</h1>' );
+                                                            the_archive_description( '<div class="taxonomy-description">', '</div>' );
+                                                      ?>
+                                                </div>
+                                                <div class="bt-cat-column">
+                                                      <?php echo '<img src="'. esc_url( $base_theme_cat_image ) .'"/>' ?>
+                                                </div>
+                                          </div>
+						</header><!-- .page-header -->
+
+						<?php
+							/* Start the Loop */
+						while ( have_posts() ) {
+
+							the_post();
+
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/category', 'content' );
+
+						}
+						?>
+
+					<?php } else { ?>
+
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+					<?php } ?>
+
+				</main><!-- #main -->
+
+					<?php base_theme_pagination(); ?>
+
+			</div><!-- #primary -->
+		</div>
+		<div class="cell small-12 medium-4 large-4">
+				<?php get_sidebar(); ?>
+		</div>
+	</div>
+</div>
+<?php
+get_footer();
---
+++ template-parts/category-content.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template part for displaying category posts.
+ *
+ * @package Base-Theme
+ */
+$bt_category_name = get_cat_name( get_queried_object_id() );
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php if ( 'post' === get_post_type() ) { ?>
+			<div class="entry-meta">
+                        Written by: <?php base_theme_posted_by(); ?> | <?php base_theme_posted_on(); ?><br> 
+                        <div class="tagList">
+                              <?php $tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'base-theme' ) );
+                                    if ( $tags_list ) {
+                                          /* translators: 1: list of tags. */
+                                         printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'base-theme' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                                    } 
+                              ?>
+                        </div>
+			</div><!-- .entry-meta -->
+		<?php } ?>
+	</header><!-- .entry-header -->
+
+	<?php
+	if ( ! is_single() && ! is_page() ) {
+
+		the_post_thumbnail( 'large' );
+
+	}
+	?>
+
+	<div class="entry-content clearfix">
+		<?php
+		if ( is_single() || is_page() ) {
+
+			the_content(
+				sprintf(
+					wp_kses(
+					/* translators: %s: Name of current post. */
+						__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'base-theme' ),
+						[
+							'span' => [
+								'class' => [],
+							],
+						]
+					),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				)
+			);
+
+			wp_link_pages(
+				[
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'base-theme' ),
+					'after'  => '</div>',
+				]
+			);
+
+		} else {
+
+			the_excerpt();
+
+		}
+		?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
